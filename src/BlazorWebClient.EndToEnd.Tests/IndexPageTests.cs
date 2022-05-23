60435fea42d0967131df7ee40e16593abc0debdd
jean <dupont>
2022-05-23 12:16:39 +0200

    Addition end-to-end tests for blazor client

---
+++ src/BlazorWebClient.EndToEnd.Tests/IndexPageTests.cs
@@ -0,0 +1,69 @@
+using FluentAssertions;
+using Microsoft.Playwright;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BlazorWebClient.EndToEnd.Tests
+{
+    [TestClass]
+    public class IndexPageTests
+    {
+        private const string PathApplication = "http://localhost:5230/";
+
+        private const string GitNameTestMessage = "jean";
+
+        private const string GitEmailTestMessage = "dupont";
+
+        private const float GenerationWaitTime = 5000f;
+
+        private string _generationFolderTestMessage = Path.Join(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Test");
+
+
+        [TestMethod]
+        public async Task Should_ShowErrorsMessage_When_EmptyForm()
+        {
+            //Arrange
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync(PathApplication);
+            await page.ClickAsync("#submitter");
+
+            //Act
+            var nbElements = await page.Locator(".validation-message").CountAsync();
+
+            //Assert
+            nbElements.Should().BeGreaterThan(0);
+        }
+
+        [TestMethod]
+        public async Task Should_Generate_When_CallApi()
+        {
+            //Arrange
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync(PathApplication);
+
+            //Act
+            await page.TypeAsync("#folder", _generationFolderTestMessage, new PageTypeOptions { Delay = 50 });
+            await page.TypeAsync("#gitname", GitNameTestMessage, new PageTypeOptions { Delay = 50 });
+            await page.TypeAsync("#gitemail", GitEmailTestMessage, new PageTypeOptions { Delay = 50 });
+            await page.ClickAsync("#submitter");
+            await page.WaitForTimeoutAsync(GenerationWaitTime);
+            //Assert
+            new DirectoryInfo(_generationFolderTestMessage).GetFiles().Length.Should().BeGreaterThan(0);
+
+            var directory = new DirectoryInfo(_generationFolderTestMessage) { Attributes = FileAttributes.Normal };
+
+            foreach (var info in directory.GetFileSystemInfos("*", SearchOption.AllDirectories))
+            {
+                info.Attributes = FileAttributes.Normal;
+            }
+
+            directory.Delete(true);
+        }
+    }
+}
