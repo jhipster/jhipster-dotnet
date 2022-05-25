4901f2aec570bb1e8098c61225c90473d0efa2a2
jean <dupont>
2022-05-25 16:01:28 +0200

    Correction for CI tests on github

--- src/JHipster.NetLite.Domain/Services/Interfaces/IGithubActionDomainService.cs
+++ src/JHipster.NetLite.Domain/Services/Interfaces/IGithubActionDomainService.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this
-
-using JHipster.NetLite.Domain.Entities;
+﻿using JHipster.NetLite.Domain.Entities;
 
 namespace JHipster.NetLite.Domain.Services.Interfaces;
 
